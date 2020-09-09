0ad0689c2e73ee7ece409875fdfa07a21a140b3f
ben <benwilcock@gmail.com>
2020-09-09 13:39:17 +0100

    Unpacked the Spring project downloaded from start.spring.io

---
+++ src/test/java/com/vmworld/demo/VMworld2020SpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmworld.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VMworld2020SpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
