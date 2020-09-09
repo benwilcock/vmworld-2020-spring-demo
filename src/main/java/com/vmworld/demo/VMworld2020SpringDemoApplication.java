b059755862ed988929f4788cfef85797db2ccde0
ben <benwilcock@gmail.com>
2020-09-09 13:39:17 +0100

    Unpacked the Spring project dpwnloaded from start.spring.io

---
+++ src/main/java/com/vmworld/demo/VMworld2020SpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vmworld.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VMworld2020SpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VMworld2020SpringDemoApplication.class, args);
+	}
+
+}
